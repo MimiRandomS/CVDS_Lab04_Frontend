1e6b92dbaabcfb924f90b381a7331527bec81af7
MimiRandomS <geronimo0scorpion@gmail.com>
2025-02-10 11:59:15 -0500

    Creando estructura

---
+++ src/main/java/edu/eci/UniReserva/UniReserva_Frontend/UniReservaFrontendApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.UniReserva.UniReserva_Frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniReservaFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniReservaFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/UniReserva/UniReserva_Frontend/UniReservaFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.UniReserva.UniReserva_Frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniReservaFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
