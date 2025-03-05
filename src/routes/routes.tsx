d495c977fd3526fe57cf965dd4fadeaaf384ad25
dadc1007 <dadica1007@gmail.com>
2025-03-04 19:03:42 -0500

    Arregle el path raiz para el proyecto

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../features/login/login";
-import Signup from "../features/signup/Signup";
+import Signup from "../features/signup/signup";
 
 function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />}></Route>
       <Route path="/signup" element={<Signup />}></Route>
     </Routes>
