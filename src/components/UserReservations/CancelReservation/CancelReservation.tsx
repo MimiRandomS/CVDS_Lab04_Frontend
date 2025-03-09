ffb70aa3d1093e0d791ec66fa33ee7cfbce5ed86
MimiRandomS <geronimo0scorpion@gmail.com>
2025-03-09 15:24:08 -0500

    Se agrego las ventanas modales de informacion y confimar cancelacion

---
+++ src/components/UserReservations/CancelReservation/CancelReservation.tsx
@@ -0,0 +1,30 @@
+import Modal from "../../Modal/Modal";
+import Styles from "./CancelReservation.module.css";
+import Button from "../../Button/Button";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+};
+
+function CancelReservation({ isOpen, onClose, onConfirm }: Props) {
+  return (
+    <Modal
+      title="¿Estás seguro de cancelar esta reserva?"
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className={Styles.container}>
+        <Button className={Styles.btn2} text="Cancelar" onClick={onClose} />
+        <Button
+          className={Styles.btn1}
+          text="Confirmar"
+          onClick={onConfirm}
+        ></Button>
+      </div>
+    </Modal>
+  );
+}
+
+export default CancelReservation;
--- src/components/UserReservations/UserReservations.tsx
+++ src/components/UserReservations/UserReservations.tsx
@@ -4,10 +4,11 @@ import Card from "../Card/Card";
 import Button from "../Button/Button";
 import IconLink from "../IconLink/IconLink";
 import DetailReservation from "./DetailReservation/DetailReservation";
+import CancelReservation from "./CancelReservation/CancelReservation";
 import infoIcon from "../../assets/public/info.ico";
 
 type Reservation = {
-  id: number;
+  id: string;
   lab: string;
   date: string;
   startTime: string;
@@ -20,8 +21,20 @@ type Props = {
 };
 
 function UserReservations({ reservations }: Props) {
+  const [reservationsList, setReservationsList] = useState<Reservation[]>(reservations);
+
   const [selectedReservation, setSelectedReservation] = useState<Reservation | null>(null);
   const [modalOpen, setModalOpen] = useState(false);
+
+  const [cancelModalOpen, setCancelModalOpen] = useState(false);
+  const [reservationToCancel, setReservationToCancel] = useState<Reservation | null>(null);
+
+  const handleCancelReservation = (reservationId: string) => {
+    // Call to backend to cancel reservation
+    setReservationsList((prevList) => prevList.filter((res) => res.id !== reservationId));
+    closeCancelModal();
+  };
+  
 
   const openModal = (reservation: Reservation) => {
     setSelectedReservation(reservation);
@@ -33,11 +46,21 @@ function UserReservations({ reservations }: Props) {
     setModalOpen(false);
   };
 
+  const openCancelModal = (reservation: Reservation) => {
+    setReservationToCancel(reservation);
+    setCancelModalOpen(true);
+  };
+
+  const closeCancelModal = () => {
+    setReservationToCancel(null);
+    setCancelModalOpen(false);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.listReservations}>
-        {reservations.map((reservation, index) => (
-          <Card key={reservation.id} title={`Reserva ${index + 1}`} className={styles.card}>
+        {reservationsList.map((reservation) => (
+          <Card key={reservation.id} title={`Reserva del ${reservation.date}`} className={styles.card}>
             <Button text="Ver" className={styles.btn} onClick={() => console.log("Abrir nuevo componente")} />
             <IconLink src={infoIcon} alt="Información reserva" onClick={() => openModal(reservation)} />
           </Card>
@@ -48,6 +71,16 @@ function UserReservations({ reservations }: Props) {
         reservation={selectedReservation}
         isOpen={modalOpen}
         onClose={closeModal}
+        onCancelReservation={() => {
+          closeModal();
+          if (selectedReservation) openCancelModal(selectedReservation);
+        }}
+      />
+
+      <CancelReservation
+        isOpen={cancelModalOpen}
+        onClose={closeCancelModal}
+        onConfirm={() => reservationToCancel && handleCancelReservation(reservationToCancel.id)}
       />
     </div>
   );
