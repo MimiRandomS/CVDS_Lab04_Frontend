7bcb6d18064363911d4b425366e63736ded239d8
dadc1007 <dadica1007@gmail.com>
2025-03-08 20:07:24 -0500

    Implemente la navegacion a la pagina principal

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Button.module.css";
 
 type Props = {
   readonly text: string;
   readonly className?: string;
+  readonly navigateTo?: string;
 };
 
-function Button({ text, className }: Props) {
+function Button({ text, className, navigateTo }: Props) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (navigateTo) {
+      navigate(navigateTo);
+    }
+  };
+
   return (
-    <button className={`${styles.btn} ${className ?? ""}`}>
+    <button
+      className={`${styles.btn} ${className ?? ""}`}
+      onClick={handleClick}
+    >
       <span>{text}</span>
     </button>
   );
