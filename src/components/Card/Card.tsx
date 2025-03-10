8f621caf21951265f71658ebd515e2c3c70de90b
MimiRandomS <geronimo0scorpion@gmail.com>
2025-03-10 08:51:42 -0500

    Cambios para idusuario en el componente

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,15 +5,19 @@ type Props = {
   readonly title: string;
   readonly children: ReactNode;
   readonly className?: string;
+  readonly titleCardClassName?: string;
+  readonly bodyCardClassName?: string;
 };
 
-function Card({ title, children, className }: Props) {
+function Card({ title, children, className, titleCardClassName, bodyCardClassName }: Props) {
   return (
     <div className={`${styles.card} ${className ?? ""}`}>
-      <div className={styles.titleCard}>
+      <div className={`${styles.titleCard} ${titleCardClassName ?? ""}`}>
         <h3>{title}</h3>
       </div>
-      <div className={styles.bodyCard}>{children}</div>
+      <div className={`${styles.bodyCard} ${bodyCardClassName ?? ""}`}>
+        {children}
+      </div>
     </div>
   );
 }
