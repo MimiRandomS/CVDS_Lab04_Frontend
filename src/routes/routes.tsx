a0715167f1395942ceca0429da9b5a76255623e6
MimiRandomS <geronimo0scorpion@gmail.com>
2025-03-12 22:07:20 -0500

    Agregando ruta de mis reservas en roots y slidebar

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Reservations from "../features/reservations/reservations";
 import Login from "../features/login/login";
 import Signup from "../features/signup/signup";
-import Account from "../features/user/account/account";
+import Account from "../features/User/account/account";
 import ChangeName from "../components/User/Update/ChangeName/changeName";
 import ChangePassword from "../components/User/Update/ChangePassword/changePassword";
 import DeleteAccount from "../components/User/Update/DeleteAccount/deleteAccount";
