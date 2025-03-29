bd56df5131241aef8535898bc35fca2898f09e13
MimiRandomS <geronimo0scorpion@gmail.com>
2025-03-29 18:06:50 -0500

    Implementacion de carrosel de reservas, se elimino un dialog inecesario y se mejoro la accesibilidad de las reservas

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,21 +3,31 @@ import { ReactNode } from "react";
 
 type Props = {
   readonly title: string;
+  readonly subtitle?: string;
   readonly children: ReactNode;
+  readonly footer?: ReactNode;
   readonly className?: string;
   readonly titleCardClassName?: string;
   readonly bodyCardClassName?: string;
+  readonly footerCardClassName?: string;
 };
 
-function Card({ title, children, className, titleCardClassName, bodyCardClassName }: Props) {
+function Card({ title, subtitle, children, footer, className, titleCardClassName, bodyCardClassName, footerCardClassName }: Props) {
   return (
     <div className={`${styles.card} ${className ?? ""}`}>
       <div className={`${styles.titleCard} ${titleCardClassName ?? ""}`}>
         <h3>{title}</h3>
+        {subtitle && (
+          <>
+            <hr className={styles.divider} />
+            <h4 className={styles.subtitle}>{subtitle}</h4>
+          </>
+        )}
       </div>
       <div className={`${styles.bodyCard} ${bodyCardClassName ?? ""}`}>
         {children}
       </div>
+      {footer && <div className={`${styles.footerCard} ${footerCardClassName ?? ""}`}>{footer}</div>}
     </div>
   );
 }
